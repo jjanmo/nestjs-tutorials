038d5dfaa0c29cc5ee58c7566b4eca96d1384031
jjanmo <jjanmo07@hanmail.net>
2021-09-27 23:23:37 +0900

    feat: add express server in app.ts

--- project01_tsnode/src/app.ts
+++ project01_tsnode/src/app.ts
@@ -1,1 +1,13 @@
-console.log('hello world🚀');
+import express, { Request, Response, Application } from 'express';
+const app: Application = express();
+const PORT: number = Number(process.env.PORT) || 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({
+    hello: 'world',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
